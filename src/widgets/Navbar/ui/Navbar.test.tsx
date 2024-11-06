27e5cc5adae7f22eb173b2c4b6060b4a919f8eec
rodion1682 <rodiontropkin17@gmail.com>
2024-11-06 16:32:10 +0200

    add visual tool

--- src/widgets/Navbar/ui/Navbar.test.tsx
+++ src/widgets/Navbar/ui/Navbar.test.tsx
@@ -1,15 +1,12 @@
 import { screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation';
+
+import { componentRender } from 'shared/lib/tests/componentRender';
 import { Navbar } from 'widgets/Navbar/ui/Navbar';
 
 describe('Navbar', () => {
 	test('Test render', () => {
-		renderWithTranslation(
-			<MemoryRouter>
-				<Navbar />
-			</MemoryRouter>
-		);
+		componentRender(<Navbar />);
+
 		expect(screen.getByTestId('navbar')).toBeInTheDocument();
 	});
 });
