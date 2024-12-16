3404cfc6b8cf0923cdce5dffb8627ad4e06ef1b5
rodion1682 <rodiontropkin17@gmail.com>
2024-12-16 19:59:48 +0200

    fix react refresh plugin

---
+++ config/build/loaders/buildBabelLoader.ts
@@ -0,0 +1,16 @@
+export function buildBabelLoader(isDev: boolean) {
+	return {
+		test: /\.(js|jsx|tsx)$/,
+		exclude: /node_modules/,
+		use: {
+			loader: 'babel-loader',
+			options: {
+				presets: ['@babel/preset-env'],
+				plugins: [
+					[],
+					isDev && require.resolve('react-refresh/babel'),
+				].filter(Boolean),
+			},
+		},
+	};
+}
